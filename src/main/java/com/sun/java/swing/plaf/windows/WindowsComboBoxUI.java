58c2bf10a180e83f7dd9b70a2acdd9a543e80697
inode- <inode@wayreth.eu.org>
2024-03-20 12:26:35 +0100

    Initial commit

---
+++ src/main/java/com/sun/java/swing/plaf/windows/WindowsComboBoxUI.java
@@ -0,0 +1,11 @@
+package com.sun.java.swing.plaf.windows;
+
+import javax.swing.plaf.basic.BasicComboBoxUI;
+
+public class WindowsComboBoxUI extends BasicComboBoxUI {
+
+    public WindowsComboBoxUI() {
+
+    }
+
+}
---
+++ src/main/java/com/sun/java/swing/plaf/windows/WindowsLookAndFeel.java
@@ -0,0 +1,37 @@
+package com.sun.java.swing.plaf.windows;
+
+import javax.swing.plaf.basic.BasicLookAndFeel;
+
+public class WindowsLookAndFeel extends BasicLookAndFeel {
+
+    static final Object HI_RES_DISABLED_ICON_CLIENT_KEY = null;
+
+    public WindowsLookAndFeel() {
+
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getID() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public boolean isNativeLookAndFeel() {
+        return true;
+    }
+
+    @Override
+    public boolean isSupportedLookAndFeel() {
+        return true;
+    }
+}
